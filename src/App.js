1d875d03434636aa1144ab43c1d2c769b71fd079
aakashmanjrekar11 <aakashmanjrekar@gmail.com>
2022-04-19 18:40:07 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import MovieCard from "./MovieCard";
+
+import htmlIcon from "./images/html-icon.png";
+import cssIcon from "./images/css-icon.png";
+import jsIcon from "./images/js-icon.png";
+import reactIcon from "./images/react-icon.png";
+
+import "./App.css";
+import SearchIcon from "./search.svg";
+
+const API_URL = "http://omdbapi.com?apikey=ec548e59";
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    setMovies(data.Search);
+  };
+
+  useEffect(() => {
+    searchMovies("Batman");
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <span>
+        <h1 style={{ display: "inline-block", verticalAlign: "middle" }}>🎬&nbsp;&nbsp;</h1>
+        <h1 style={{ display: "inline-block", verticalAlign: "middle" }} className="movieland">
+          Movieland
+        </h1>
+      </span>
+
+      <div className="search">
+        <input
+          placeholder="Search for movies"
+          value={searchTerm}
+          onChange={(event) => {
+            setSearchTerm(event.target.value);
+          }}
+        />
+        <img
+          src={SearchIcon}
+          alt="search-icon"
+          onClick={() => {
+            searchMovies(searchTerm);
+          }}
+        />
+      </div>
+
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
+
+      <div className="madewith">
+        <h2 style={{ display: "inline", verticalAlign: "middle" }}>Made with ❤️ by Aakash M. using </h2>
+        <img style={{ display: "inline", verticalAlign: "middle" }} className="icon" src={reactIcon} alt="react-icon" />
+        <img style={{ display: "inline", verticalAlign: "middle" }} className="icon" src={htmlIcon} alt="html-icon" />
+        <img style={{ display: "inline", verticalAlign: "middle" }} className="icon" src={cssIcon} alt="css-icon" />
+        <img style={{ display: "inline", verticalAlign: "middle" }} className="icon" src={jsIcon} alt="js-icon" />
+      </div>
     </div>
   );
 }
